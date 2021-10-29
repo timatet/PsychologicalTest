c991f8be995e41555c10f3d651112e915811a990
Ульяна Малеева <maleeva012002@gmail.com>
2021-10-29 16:19:41 +0300

    Include all changes

---
+++ Wpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] TestNames = {
+                "Дианостика мотивационной структуры личности",
+                "Личностные творческие характеристики" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListOfTestNames.ItemsSource = TestNames;
+        }
+        private void ButtonContinue_Click(object sender, RoutedEventArgs e)
+        {
+            string NameOfTest = ListOfTestNames.SelectedItem.ToString();
+            // запустить нужный тест
+        }
+    }
+}
