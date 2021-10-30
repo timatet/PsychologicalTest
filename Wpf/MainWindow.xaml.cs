f4dadaeb381ca64ff80f83daafa36760dba32703
Ульяна Малеева <maleeva012002@gmail.com>
2021-10-30 13:53:08 +0300

    include changes

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -19,25 +19,39 @@ namespace Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string[] TestNames = {
-                "Дианостика мотивационной структуры личности",
-                "Личностные творческие характеристики" };
+        //private string[] TestNames = {
+        //        "Дианостика мотивационной структуры личности",
+        //        "Личностные творческие характеристики" };
+        InformaitionAboutTests InfAT = new InformaitionAboutTests();
         public MainWindow()
         {
             InitializeComponent();
-            ListOfTestNames.ItemsSource = TestNames;
+            ListOfTestNames.ItemsSource = InfAT.TestNames;
         }
         private void ButtonContinue_Click(object sender, RoutedEventArgs e)
         {
-            string NameOfTest = ListOfTestNames.SelectedItem.ToString();
-            // запустить нужный тест
-            if (NameOfTest == TestNames[0])
+            if (ListOfTestNames.SelectedItem != null)
             {
+                // запустить нужный тест
+                string NameOfTest = ListOfTestNames.SelectedItem.ToString();
                 DescriptionAndInstruction DesAndIns = new DescriptionAndInstruction();
-                this.Close();
-                DesAndIns.Show();
+                        this.Close();
+                        DesAndIns.Show();
+                switch (NameOfTest)
+                {
+                    case "Дианостика мотивационной структуры личности":
+                        DesAndIns._lableNameOfTest.Content = "Дианостика мотивационной структуры личности";
+                        DesAndIns.DescriptionOfTest.Text = InfAT.TestDescription[0];                            
+                        DesAndIns.InstructionOfTest.Text = InfAT.TestInstruction[0];
+                        break;
+                    case "Личностные творческие характеристики":
+                        DesAndIns._lableNameOfTest.Content = "Личностные творческие характеристики";
+                        DesAndIns.DescriptionOfTest.Text = InfAT.TestDescription[1];                            
+                        DesAndIns.InstructionOfTest.Text = InfAT.TestInstruction[1];
+                        break;
+                }
             }
-                
+
         }
     }
 }
